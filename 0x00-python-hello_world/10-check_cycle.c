c63de85ceee4f90cbe0b883bee1159e33692bc4b
Lekarri <lekarrikaila567@gmail.com>
2022-11-29 02:02:05 -0800

    debugged

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -4,28 +4,25 @@
 /**
  * check_cycle - checks presence of cycle
  * @list: pointer to listint_t
- * @Return: int
+ * @Return:0 if there is no cycle, 1 if cycle exists
  */
 
 int check_cycle(listint_t *list)
 {
-listint_t *jump, *node;
+	listint_t *list_fast, *list_slow;
 
-if (!list || !list->next)
+	if (!list)
+		return (0);
+
+	list_fast = list_slow = list;
+	while (list_fast)
+	{
+		if (list_fast->next == NULL || (list_fast->next)->next ==NULL)
+			return (0);
+		list_fast = (list_fast->next)->next;
+		if (list_slow == list_fast)
+			return (1);
+		list_slow = list_slow->next;
+	}
 	return (0);
-
-node = list;
-jump = list->next;
-
-while (jump && jump->next && node && node->next)
-{
-if (jump == node)
-return (1);
-jump = jump->next->next;
-if (!jump)
-
-break;
-node = node->next;
-}
-return (0);
 }
