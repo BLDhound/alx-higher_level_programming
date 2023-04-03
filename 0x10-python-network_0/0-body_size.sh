8d8d84a5f19b3f464074e4828033dc9c86339aab
Lekarri <lekarrikaila567@gmail.com>
2023-04-03 04:32:05 -0700

    changes

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
-
-# Get the URL from the command line argument
-url=$1
-
-# Use curl to send a request to the URL and store the response body in a variable
-response=$(curl -s $url)
-
-# Get the size of the response body in bytes and display it
-size=$(echo -n $response | wc -c)
-echo "The size of the response body is $size bytes."
+# catch content-length
+curl -sI "$1" | grep "Content-Length" | cut -d ' ' -f2
